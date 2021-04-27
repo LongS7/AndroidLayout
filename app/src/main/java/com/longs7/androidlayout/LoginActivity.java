0d72705a9336df2a732e56353c37133d913e6993
longs7 <longkeyvic1@gmail.com>
2021-04-27 21:55:01 +0700

    update layout

--- app/src/main/java/com/longs7/androidlayout/LoginActivity.java
+++ app/src/main/java/com/longs7/androidlayout/LoginActivity.java
@@ -3,6 +3,8 @@ package com.longs7.androidlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView tvUsername = findViewById(R.id.txtUserName);
+        TextView tvPassword = findViewById(R.id.txtPassword);
+
+        findViewById(R.id.btnLogin).setOnClickListener(v -> {
+            Toast.makeText(this, "Username: " + tvUsername.getText() + ", password: " + tvPassword.getText(), Toast.LENGTH_LONG).show();
+        });
     }
 }
